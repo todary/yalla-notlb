edace0fcafcc26d2fca9ed2b7e5c545748d64b50
abeer <abeer.elhout@gmail.com>
2016-05-20 03:21:32 +0200

    configration test

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,9 @@ module Yalanotlob
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    # Heroku requires this to be false
+    config.assets.initialize_on_precompile=false
   end
 end
+
+
