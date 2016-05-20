fa5615350cbe14024a14211b5e0c0ead9f0b7431
InasAbdelfattah <engenas21@gmail.com>
2016-05-20 14:22:24 +0200

    upload image

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160520073625) do
+ActiveRecord::Schema.define(version: 20160520120218) do
 
   create_table "friends", force: :cascade do |t|
     t.integer  "friend_id",  limit: 4
@@ -80,11 +80,11 @@ ActiveRecord::Schema.define(version: 20160520073625) do
   create_table "orders", force: :cascade do |t|
     t.string   "name",       limit: 255
     t.string   "resturant",  limit: 255
-    t.boolean  "status"
+    t.boolean  "status",                 default: true
     t.integer  "user_id",    limit: 4
     t.string   "image",      limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
   end
 
   add_index "orders", ["user_id"], name: "index_orders_on_user_id", using: :btree
@@ -105,7 +105,12 @@ ActiveRecord::Schema.define(version: 20160520073625) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
     t.string   "last_sign_in_ip",        limit: 255
-
+    t.string   "provider",               limit: 255
+    t.string   "uid",                    limit: 255
+    t.string   "avatar_file_name",       limit: 255
+    t.string   "avatar_content_type",    limit: 255
+    t.integer  "avatar_file_size",       limit: 4
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
