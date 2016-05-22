0b124e455e2e6859a9f72ac48861df8665f89fb4
abeer <abeer.elhout@gmail.com>
2016-05-22 08:32:30 +0200

    edit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,9 +34,9 @@ Rails.application.configure do
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.raise_delivery_errors = true
   config.assets.raise_runtime_errors = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000'}
+  # config.action_mailer.default_url_options = { :host => 'localhost:3000'}
 =begin
 config.action_mailer.delivery_method = :smtp
 
