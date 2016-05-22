aef029837426333a2b81806673401aa15c0d981d
InasAbdelfattah <engenas21@gmail.com>
2016-05-22 09:24:13 +0200

    reset password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,6 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
@@ -77,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { :host => "yallanotloborder.herokuapp.com" }
+
 end
